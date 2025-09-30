d37a38f842309384bec062faf5707d7601f272b0
George Zhang <georgezhang02@hotmail.com>
2025-09-30 00:55:50 -0700

    Comments

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -15,21 +15,24 @@ import (
 func TestClient_InsertAndFind(t *testing.T) {
 	addr := "127.0.0.1:50051"
 
+	// Start the server
 	srv := proxy.NewServer()
 	srv.Start(addr)
 	t.Cleanup(srv.Stop)
 
+	// Create a client
 	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		t.Fatalf("failed NewClient: %v", err)
 	}
 	t.Cleanup(func() { _ = conn.Close() })
-
 	client := pb.NewMongoProxyClient(conn)
 
+	// Create a context
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	t.Cleanup(cancel)
 
+	// Insert a document
 	insResp, err := client.Insert(ctx, &pb.InsertRequest{Db: "test", Collection: "col"})
 	if err != nil {
 		t.Fatalf("Insert error: %v", err)
@@ -38,6 +41,7 @@ func TestClient_InsertAndFind(t *testing.T) {
 		t.Fatalf("unexpected Insert response: %#v", insResp)
 	}
 
+	// Find a document
 	findResp, err := client.Find(ctx, &pb.FindRequest{Db: "test", Collection: "col"})
 	if err != nil {
 		t.Fatalf("Find error: %v", err)
